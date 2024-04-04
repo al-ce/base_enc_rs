5b558bc7a2c43591ae09edf8389498bc3fde25b4
al-ce <aceserani@gmail.com>
2024-04-04 16:29:08 -0500

    refactor: add Counter struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,59 @@
 use std::env;
 use std::fs::File;
+use std::io::prelude::*;
 use std::io::{self};
+use std::process;
 
 pub fn parse_args(args: &[String]) -> Result<String, &'static str> {
     match args.len() {
         1 => {
-            println!("No arguments");
+            eprintln!("No arguments");
             Ok("-".to_string())
         }
         2 => {
-            println!("Processing one argument");
+            eprintln!("Processing one argument");
             Ok(env::args().nth(1).unwrap())
         }
         _ => Err("Too many arguments"),
     }
 }
 
-pub fn get_input(filename: &String) -> Box<dyn Iterator<Item = io::Result<String>>> {
-    if filename == "-" {
+fn open_file(filename: String) -> File {
+    File::open(filename).unwrap_or_else(|err| {
+        eprintln!("Could not open file : {err}");
+        process::exit(1);
+    })
+}
+
+pub fn get_lines(filename: String) -> Box<dyn Iterator<Item = io::Result<String>>> {
+    let lines: Box<dyn Iterator<Item = io::Result<String>>> = if filename == "-" {
         Box::new(io::BufReader::new(io::stdin()).lines())
     } else {
-        let f = File::open(filename);
+        let f = open_file(filename);
         Box::new(io::BufReader::new(f).lines())
+    };
+    lines
+}
+
+
+pub struct Counter {
+    pub count: usize,
+    limit: usize,
+}
+
+impl Counter {
+    pub fn init(limit: usize) -> Counter {
+        Counter { count: 0, limit }
+    }
+
+    pub fn increment(&mut self) {
+        self.count += 1;
+    }
+
+    pub fn check_reset(&mut self) -> bool {
+        if self.count >= self.limit {
+            self.count = 0;
+        }
+        self.count == 0
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 use std::env;
-use std::fs::File;
 use std::io::{self};
 use std::process;
 
-struct WrapCounter {
-    count: usize,
-    limit: usize,
-}
-
-impl WrapCounter {
-    fn init(&self, limit: usize) {
-        WrapCounter{count: 0, limit}
-    }
-    fn need_wrap(&self) -> bool {
-        self.count += 1;
-        if self.count >= self.limit {
-            self.count = 0;
-            return true;
-        }
-        false
-    }
-}
+const WRAP_LIMIT: usize = 76;
+const BYTE_LIMIT: usize = 3;
 
 fn main() -> io::Result<()> {
     let b64a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
@@ -31,30 +14,22 @@ fn main() -> io::Result<()> {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    eprintln!("filename: {:?}", filename);
 
-    let lines: Box<dyn Iterator<Item = io::Result<String>>> = if filename == "-" {
-        Box::new(io::BufReader::new(io::stdin()).lines())
-    } else {
-        let f = File::open(filename)?;
-        Box::new(io::BufReader::new(f).lines())
-    };
+    eprintln!("filename: {:?}", filename);
+    let lines = base64_rs::get_lines(filename);
 
     eprintln!("Beginning base 64 encoding");
 
     let mut ac: u16 = 0;
     let mut ac_bits = 0;
-    let mut byte_counter = 0;
-    // let mut wrap_counter = 0;
-    let wrap_counter = WrapCounter.init();
+    let mut byte_counter = base64_rs::Counter::init(BYTE_LIMIT);
+    let mut wrap_counter = base64_rs::Counter::init(WRAP_LIMIT);
 
     for line in lines {
         let line = line.unwrap();
         for byte in line.bytes() {
-            byte_counter += 1;
-            if byte_counter == 3 {
-                byte_counter = 0;
-            }
+            byte_counter.increment();
+            byte_counter.check_reset();
 
             let byte: u16 = (byte).into();
             ac <<= 8;
@@ -68,30 +43,26 @@ fn main() -> io::Result<()> {
 
                 print!("{}", &b64a[idx..idx + 1]);
 
-                if wrap_counter.inc() {
+                wrap_counter.increment();
+                if wrap_counter.check_reset() {
                     print!("\n")
                 }
-                // wrap_counter += 1;
-                // if wrap_counter >= 76 {
-                //     wrap_counter = 0;
-                //     print!("\n");
-                // }
             }
 
             assert!((0..6).contains(&ac_bits));
         }
     }
 
-    if byte_counter != 0 {
+    if byte_counter.count != 0 {
         let idx: usize = (ac << (6 - ac_bits)).into();
         print!("{}", &b64a[idx..idx + 1]);
 
-        let count = (3 - byte_counter) * 8 / 6;
+        let count = (3 - byte_counter.count) * 8 / 6;
         for _ in 0..count {
             print!("=");
         }
 
-        if wrap_counter != 0 {
+        if wrap_counter.count != 0 {
             print!("\n");
         }
     }
